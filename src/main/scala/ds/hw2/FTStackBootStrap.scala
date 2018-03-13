afa964b17f1c3b14b79a0f54c64b5536ece6ed15
Hasil Sharma <Hasil.Sharma@colorado.edu>
2018-03-12 23:39:58 -0600

    Updated BootStrap Script

--- src/main/scala/ds/hw2/FTStackBootStrap.scala
+++ src/main/scala/ds/hw2/FTStackBootStrap.scala
@@ -21,6 +21,45 @@ object FTStackBootStrap extends App with Logging {
   ckiteLeader.start()
   // Starting the leader
 
+  var follower1 = CKiteBuilder()
+    .listenAddress("localhost:9092")
+    .rpc(FinagleThriftRpc)
+    .stateMachine(new FTStack())
+    .build
+
+  follower1.start()
+
+  ckiteLeader.addMember("localhost:9092")
+
+  var follower2 = CKiteBuilder()
+    .listenAddress("localhost:9093")
+    .rpc(FinagleThriftRpc)
+    .stateMachine(new FTStack())
+    .build
+
+  follower2.start()
+
+  ckiteLeader.addMember("localhost:9093")
+
+  var follower3 = CKiteBuilder()
+    .listenAddress("localhost:9094")
+    .rpc(FinagleThriftRpc)
+    .stateMachine(new FTStack())
+    .build
+
+  follower3.start()
+
+  ckiteLeader.addMember("localhost:9094")
+
+  var follower4 = CKiteBuilder()
+    .listenAddress("localhost:9095")
+    .rpc(FinagleThriftRpc)
+    .stateMachine(new FTStack())
+    .build
+
+  follower4.start()
+
+  ckiteLeader.addMember("localhost:9095")
 
   var stackIdFuture: Future[Int] = ckiteLeader.write(sCreate(1))
   while (!stackIdFuture.isCompleted) {
@@ -67,14 +106,27 @@ object FTStackBootStrap extends App with Logging {
   sizeFuture = ckiteLeader.read(sSize(1))
   sizeFuture.foreach { size => println(s"6: Size of stack with id: 1 is ${size.getOrElse(None)}") }
 
+//  scala.io.StdIn.readLine()
   var popFuture: Future[Int] = ckiteLeader.write(sPop(2))
   while(!popFuture.isCompleted){
 
   }
 
+
   popFuture.foreach {elem => println(s"7: Popped stack with id: 2 = $elem")}
 
+
+  ckiteLeader.removeMember("localhost:9094")
+  ckiteLeader.removeMember("localhost:9095")
+
   sizeFuture = ckiteLeader.read(sSize(2))
+  while(!sizeFuture.isCompleted){
+
+  }
   sizeFuture.foreach { size => println(s"8: Size of stack with id: 2 is ${size.getOrElse(None)}") }
+
+  ckiteLeader.stop()
+  follower1.stop()
+  follower2.stop()
 
 }
